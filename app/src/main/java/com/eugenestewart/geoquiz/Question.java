899957a87456f6f93b0c1ad1322bb0c3ea55768d
Eugene <Eugenecs326648@yahoo.com>
2016-09-13 16:54:10 -0500

    update

---
+++ app/src/main/java/com/eugenestewart/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package com.eugenestewart.geoquiz;
+
+/**
+ * Created by in0418gq on 9/13/16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+
+        mTextResId=textResId;
+        mAnswerTrue=answerTrue;
+    }
+    public int getTextResId(){
+        return mTextResId;
+    }
+    public void setTextResId(int textResId){
+        mTextResId=textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
